f9f63277ae408383133b2cd56d1593e89b0e2f8a
Ankit Mishra <ankitmishra1701@gmail.com>
2017-05-10 17:51:28 +0530

    initial

---
+++ Section-1/Video-1.4/SpringSecurity-LDAP-Embedded/src/main/java/com/demo/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author ankidaemon
+ *
+ */
+@Configuration
+@EnableWebSecurity
+@ComponentScan(basePackages = "com.demo.config")
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+		.ldapAuthentication()
+		.contextSource()
+			.root("dc=packt,dc=com")
+			.ldif("classpath:packt.ldif")
+		.and()
+			.userDnPatterns("uid={0},ou=finance")
+			.groupSearchBase("ou=groups");
+	}
+
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests()
+		.regexMatchers("/chief/.*").hasRole("ADMIN")
+		.regexMatchers("/agent/.*").access("hasRole('USER')")
+		.anyRequest()
+				.authenticated()
+				.and().httpBasic()	
+				.and().requiresChannel().anyRequest().requiresSecure();
+
+		http.formLogin().loginPage("/login").permitAll();
+		http.logout().logoutSuccessUrl("/");
+		http.exceptionHandling().accessDeniedPage("/accessDenied");
+		
+	}
+
+}
---
+++ Section-1/Video-1.4/SpringSecurity-LDAP-Embedded/src/main/java/com/demo/web/HomeController.java
@@ -0,0 +1,40 @@
+package com.demo.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author ankidaemon
+ *
+ */
+@Controller
+public class HomeController {
+     
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public ModelAndView visitHome() {
+    	ModelAndView mav = new ModelAndView("home");
+        return mav;
+    }
+    
+    @RequestMapping(value = "/chief/updateProfile", method = RequestMethod.GET)
+   	public ModelAndView updatePage(HttpServletRequest request) {
+   		ModelAndView mav = new ModelAndView();
+   		mav.setViewName("chiefUpdate");
+   		return mav;
+   	}
+     
+       @RequestMapping(value="/accessDenied", method = RequestMethod.GET)
+       public ModelAndView accessDenied() {
+       	ModelAndView mav = new ModelAndView("accessDenied");
+           return mav;
+       }
+    
+}
